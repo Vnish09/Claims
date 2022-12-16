fd22d456326a94b4a23d0f7e174b70da0932b8f6
Vnish09 <120713964+Vnish09@users.noreply.github.com>
2022-12-16 15:16:01 +0530

    Add files via upload

---
+++ authorization/src/main/java/com/authorizationservice/authorization/AuthorizationApplication.java
@@ -0,0 +1,34 @@
+package com.authorizationservice.authorization;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.authorizationservice.authorization.model.AuthenticationRequest;
+import com.authorizationservice.authorization.repository.AuthRequestRepo;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class AuthorizationApplication implements CommandLineRunner{
+	
+	@Autowired
+	AuthRequestRepo authRequestRepo;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception{
+		AuthenticationRequest user1 = new AuthenticationRequest("aniii","iii12");
+		AuthenticationRequest user2 = new AuthenticationRequest("anish","12ani");
+		//AuthenticationRequest user3 = new AuthenticationRequest("hello","root");
+		
+		authRequestRepo.save(user1);
+		authRequestRepo.save(user2);
+	}
+
+}
